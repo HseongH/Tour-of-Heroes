93cfa3e330051788feb1f18691153ea6edee2edc
HseongH <hh4518@gmail.com>
2022-10-15 21:29:58 +0900

    목록 상태를 나타내는 메세지 컴포넌트 추가

--- src/app/heroes/hero.service.ts
+++ src/app/heroes/hero.service.ts
@@ -5,14 +5,17 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero.model';
 import { Heroes } from './mock-heroes';
 
+import { MessageService } from '../messages/message.service';
+
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor() {}
+  constructor(private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(Heroes);
+    this.messageService.addMessage('HeroService: fetched heroes');
 
     return heroes;
   }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero.model';
 import { HeroService } from './hero.service';
 
+import { MessageService } from '../messages/message.service';
+
 @Component({
   selector: 'hero-heroes',
   templateUrl: './heroes.component.html',
@@ -13,7 +15,10 @@ export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) {}
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
 
   ngOnInit(): void {
     this.getHeroes();
@@ -21,6 +26,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.addMessage(
+      `HeroesComponent: Selected hero id=${hero.id}`
+    );
   }
 
   getHeroes(): void {
